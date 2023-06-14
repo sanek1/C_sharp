4b675cd44c772eb3c7354d40e7ad3b049d21b91d
Alex <a.maslov@inostudio.com>
2023-06-14 19:03:03 +0300

    #lesson1

---
+++ Task3/Program.cs
@@ -0,0 +1,4 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+var number = int.Parse(Console.ReadLine());
+var res = number % 2 == 0 ? "Yes" : "No";
+Console.WriteLine(res);
---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число(N), а на выходе показывает все чётные числа от 1 до N.
+var n = int.Parse(Console.ReadLine());
+int[] res = new int[n]  ;
+int count = 0;
+for (int i = 1; i < n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            res[count]=i;
+            count++;
+        }
+    }
+res= res.Except(new int[] { 0}).ToArray();
+
+for (int i = 0; i < res.Length; i++)
+    {
+        Console.Write(res[i] + " ");
+    }
+
