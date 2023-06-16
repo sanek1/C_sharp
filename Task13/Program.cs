b8929958abcae849da169c6fb3e7e69dcc04e7f6
Alex <a.maslov@inostudio.com>
2023-06-16 16:40:49 +0300

    #lesson2

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int? FindNumber(int number, int count){
+    var stringNumber = number.ToString();
+    if(count > stringNumber.Length)  return null;
+    return int.Parse(stringNumber[count-1].ToString());
+}
+
+var number = int.Parse(Console.ReadLine());
+var res = FindNumber(number, 3);
+Console.WriteLine(res == null ? "третьей цифры нет":$"{res}");
