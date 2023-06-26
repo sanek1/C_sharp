c452b5540d8a59dee265d904e9954178f836801c
Alex <a.maslov@inostudio.com>
2023-06-26 19:01:30 +0300

    #lesson4

---
+++ Task29/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("array = ");
+var n = 8;//Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [n];
+for (int i = 0;i<array.Length;i++){
+    array[i] = i;
+    Console.Write(array[i] + " ");
+}
