9e4404f6df4f77b108477bd7644564fba2212785
Alex <a.maslov@inostudio.com>
2023-06-29 18:58:48 +0300

    #lesson6

---
+++ Task41/Program.cs
@@ -0,0 +1,6 @@
+﻿
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.Write("array -> : ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = array.Count(x => x > 0);
+Console.WriteLine($"result = {count}");
