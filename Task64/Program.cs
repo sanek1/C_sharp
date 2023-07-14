09b46fcd1a8bd75514bdff03cec1decec70cb7ff
Alex <a.maslov@inostudio.com>
2023-07-14 09:15:19 +0300

    #lesson9

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"result = {0}",Sum(m - 1, n));
+
+int Sum(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + Sum(m, n);
+        return res;
+    }
+}
